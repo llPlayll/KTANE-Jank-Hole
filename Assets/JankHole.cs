d881ad335d942c8d2c83a42e76df7cd7a07b8d95
llPlayll <129607915+llPlayll@users.noreply.github.com>
2023-06-24 04:01:26 +0500

    6

---
+++ Assets/JankHole.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JankHole : MonoBehaviour {
+
+	void Start ()
+    {
+        int thing = 1;
+        string balls = $"Hi {thing}";
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
