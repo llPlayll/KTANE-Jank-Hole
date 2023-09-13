be95728f18cd12fe564d1d14b0d2caeb92cb8743
llPlayll <129607915+llPlayll@users.noreply.github.com>
2023-09-13 11:58:51 +0500

    Added Key calculation.

--- Assets/JankHole.cs
+++ Assets/JankHole.cs
@@ -25,6 +25,7 @@ public class JankHole : MonoBehaviour
     List<string> fullColorNames = new List<string> { "Red", "Green", "Blue", "Cyan", "Magenta", "Yellow", "White" };
 
     string X, Y, Z;
+    string KeyA, KeyB = "";
     int[] colorIndexesArray = new int[10];
 
     int colorSequenceLength = 9;
@@ -37,8 +38,9 @@ public class JankHole : MonoBehaviour
     void Start()
     {
         ColorBlindText.gameObject.SetActive(Colorblind.ColorblindModeActive);
-        CalculateGoalLetters();
         GenerateColors();
+        CalculateGoalLetters();
+        CalculateKeys();
 
         Module.OnActivate += ModuleOnActivate;
     }
@@ -75,7 +77,150 @@ public class JankHole : MonoBehaviour
             default:
                 break;
         }
-        CustomLog($"Goal letters are {X}, {Y}, and {Z}.");
+        Log($"Goal letters are {X}, {Y}, and {Z}.");
+    }
+
+    void CalculateKeys()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            int currentColor = colorIndexesArray[i];
+            switch (currentColor)
+            {
+                case 0:
+                    if (Bomb.GetSerialNumber()[5] % 2 == 0)
+                    {
+                        KeyA += "EVEN";
+                    }
+                    else
+                    {
+                        KeyA += "ODD";
+                    }
+                    break;
+                case 1:
+                    if (!String.IsNullOrEmpty(KeyA))
+                    {
+                        KeyA = Rearrange(KeyA, true);
+                    }
+                    break;
+                case 2:
+                    if (!String.IsNullOrEmpty(KeyA))
+                    {
+                        KeyA = Reverse(KeyA.Substring(0, KeyA.Length / 2)) + KeyA.Substring(KeyA.Length / 2);
+                    }
+                    break;
+                case 3:
+                    if (!String.IsNullOrEmpty(KeyA))
+                    {
+                        KeyA = Reverse(KeyA);
+                    }
+                    break;
+                case 4:
+                    KeyA = AppendIndicators(KeyA, true);
+                    break;
+                case 5:
+                    KeyA += X + Y + Z;
+                    break;
+                case 6:
+                    List<char> serialNumLetters = Bomb.GetSerialNumberLetters().ToList();
+                    for (int j = 0; j < serialNumLetters.Count; j++)
+                    {
+                        KeyA += serialNumLetters[j].ToString();
+                    }
+                    break;
+            }
+            if (!String.IsNullOrEmpty(KeyA))
+            {
+                Log($"Key A, Applied the {fullColorNames[currentColor]} color, Key A is now {KeyA}");
+            }
+            else
+            {
+                Log($"Key A, Applied the {fullColorNames[currentColor]} color, Key A is now \"\"");
+            }    
+        }
+        for (int i = 5; i < 10; i++)
+        {
+            int currentColor = colorIndexesArray[i];
+            switch (currentColor)
+            {
+                case 0:
+                    if (!String.IsNullOrEmpty(KeyB))
+                    {
+                        KeyB = Reverse(KeyB);
+                    }
+                    break;
+                case 1:
+                    if (!String.IsNullOrEmpty(KeyB))
+                    {
+                        KeyB = KeyB.Substring(0, KeyB.Length / 2) + Reverse(KeyB.Substring(KeyB.Length / 2));
+                    }
+                    break;
+                case 2:
+                    KeyB += KeyA;
+                    break;
+                case 3:
+                    if (!String.IsNullOrEmpty(KeyB))
+                    {
+                        KeyB = Rearrange(KeyB, false);
+                    }
+                    break;
+                case 4:
+                    if (!String.IsNullOrEmpty(KeyA))
+                    {
+                        if (KeyA.Length % 2 == 1)
+                        {
+                            KeyB += KeyA[KeyA.Length / 2].ToString();
+                        }
+                        else
+                        {
+                            KeyB += KeyA[0].ToString();
+                        }
+                    }
+                    break;
+                case 5:
+                    KeyB = AppendIndicators(KeyB, false);
+                    break;
+                case 6:
+                    List<int> serialNumDigits = Bomb.GetSerialNumberNumbers().ToList();
+                    for (int j = 0; j < serialNumDigits.Count; j++)
+                    {
+                        KeyB += alphabet[serialNumDigits[j]];
+                    }
+                    break;
+            }
+            if (!String.IsNullOrEmpty(KeyB))
+            {
+                Log($"Key B, Applied the {fullColorNames[currentColor]} color, Key B is now {KeyB}");
+            }
+            else
+            {
+                Log($"Key B, Applied the {fullColorNames[currentColor]} color, Key B is now \"\"");
+            }
+        }
+
+        KeyA += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        KeyB += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string TempKA = KeyA;
+        string TempKB = KeyB;
+        KeyA = KeyB = "";
+
+        for (int i = 0; i < TempKA.Length; i++)
+        {
+            if (!KeyA.Contains(TempKA[i]))
+            {
+                KeyA += TempKA[i];
+            }
+        }
+
+        for (int i = 0; i < TempKB.Length; i++)
+        {
+            if (!KeyB.Contains(TempKB[i]))
+            {
+                KeyB += TempKB[i];
+            }
+        }
+
+        Log($"After doing all modifications, Key A is {KeyA} and Key B is {KeyB}");
     }
 
     void GenerateColors()
@@ -106,10 +251,10 @@ public class JankHole : MonoBehaviour
                 colorLog += ".";
             }
         }
-        CustomLog($"Generated color sequence is: {colorLog}");
+        Log($"Generated color sequence is: {colorLog}");
     }
 
-    void CustomLog(string arg)
+    void Log(string arg)
     {
         Debug.Log($"[Jank Hole #{ModuleId}] {arg}");
     }
@@ -117,6 +262,73 @@ public class JankHole : MonoBehaviour
     void ModuleOnActivate()
     {
         StartCoroutine("ColorCycle");
+    }
+
+    string Rearrange(string key, bool oddFirst)
+    {
+        List<char> keyEvenPositions = new List<char>();
+        List<char> keyOddPositions = new List<char>();
+        for (int j = 0; j < key.Length; j++)
+        {
+            if ((j + 1) % 2 == 0)
+            {
+                keyEvenPositions.Add(key[j]);
+            }
+            else
+            {
+                keyOddPositions.Add(key[j]);
+            }
+        }
+        key = "";
+        if (oddFirst)
+        {
+            for (int j = 0; j < keyOddPositions.Count; j++)
+            {
+                key += keyOddPositions[j].ToString();
+            }
+            for (int j = 0; j < keyEvenPositions.Count; j++)
+            {
+                key += keyEvenPositions[j].ToString();
+            }
+        }
+        else
+        {
+            for (int j = 0; j < keyEvenPositions.Count; j++)
+            {
+                key += keyEvenPositions[j].ToString();
+            }
+            for (int j = 0; j < keyOddPositions.Count; j++)
+            {
+                key += keyOddPositions[j].ToString();
+            }
+        }
+        return key;
+    }
+
+    string Reverse(string key)
+    {
+        char[] charArray = key.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+    
+    string AppendIndicators(string key, bool lit)
+    {
+        List<string> indicatorList = new List<string>();
+        if (lit)
+        {
+            indicatorList = Bomb.GetOnIndicators().ToList();
+        }
+        else
+        {
+            indicatorList = Bomb.GetOffIndicators().ToList();
+        }
+        indicatorList.Sort();
+        for (int j = 0; j < indicatorList.Count; j++)
+        {
+            key += indicatorList[j];
+        }
+        return key;
     }
 
     IEnumerator ColorCycle()
